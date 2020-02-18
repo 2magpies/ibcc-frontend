1c437ad98b7f1463b52c4c360a8931b6e5d695ec
bwilson19 <brendan.wilson19@gmail.com>
2020-02-18 15:58:02 -0800

    styling updates

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import {CardColumns, Card} from 'react-bootstrap'
 
 function Results(props) {
   const { newResults } = props;
@@ -9,16 +11,26 @@ function Results(props) {
   }
 
   return (
-    <div className="results">
-      {newResults.map(event => (
-        <div key={event._id} className="event">
-          <Link to={`/${event._id}`}>
-            <p>{event.name}</p>
-          </Link>
-        </div>
-      ))}
+    <div className="resultsGrid">
+      <CardColumns>
+        {newResults.map(event => (
+          <Card key={event._id}>
+            <Link to={`/${event._id}`}>
+              <Card.Img variant="top" src={event.imageUrl} alt={event.name} />
+              <Card.Body>
+                <Card.Title>{event.name}</Card.Title>
+                <Card.Text>
+                  <p>{moment(event.date).format('ddd, MMM Do YYYY')}</p>
+                  <p>{event.location}</p>
+                </Card.Text>
+              </Card.Body>
+            </Link>
+          </Card>
+        ))}
+      </CardColumns>
     </div>
   );
 }
 
 export default Results;
+
