7380360964573fdc1b47e84cf93d044cc8556d6b
Mysta3 <myshawnestallings@gmail.com>
2020-02-18 08:55:07 -0800

    laid base styles and added redirect functionality

--- src/components/ManageUser/ManageUser.js
+++ src/components/ManageUser/ManageUser.js
@@ -28,37 +28,39 @@ function ManageUser() {
     userData.email = event.target['email'].value;
     // userData.city = event.target['city'].value;
     // userData.state = event.target['state'].value;
-
     postNewUser(userData);
+    window.location.href = 'http://localhost:3000';
   };
   return (
     <div className="postUser">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name</label>
-          <input
-            type="text"
-            placeholder="Enter your name"
-            name="name"
-            required
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            placeholder="Enter email here"
-            name="email"
-            required
-          />
-        </div>
-        {/* <div>
+        <div className="userDetails">
+          <div className="userNameField">
+            <label>Name: </label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              required
+            />
+          </div>
+          <div className="emailField">
+            <label>Email: </label>
+            <input
+              type="text"
+              placeholder="Enter email here"
+              name="email"
+              required
+            />
+          </div>
+          {/* <div>
           <label>Location</label>
           <input type="text" placeholder="City" name="city" />
           <input type="text" placeholder="State" name="state" />
         </div> */}
-        <div>
-          <button type="submit">Submit User</button>
+          <div>
+            <button type="submit">Submit User</button>
+          </div>
         </div>
       </form>
     </div>
