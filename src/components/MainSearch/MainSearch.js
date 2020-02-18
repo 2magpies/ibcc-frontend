5fa6cbf7bbe5a403778e6491a7c3fcf0006056a0
tiffhuddleston <tiffhuddleston@gmail.com>
2020-02-18 15:17:16 -0600

    edited main search

--- src/components/MainSearch/MainSearch.js
+++ src/components/MainSearch/MainSearch.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Search from '../Search/Search';
 
 function MainSearch(props) {
-    return (
-        <div className="MainSearch">
-           <Search />
-        </div>
-    );
+  const { handleSubmit, handleChange, searchString, lastSearch } = props;
+  return (
+    <div className="MainSearch">
+      <Search
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        searchString={searchString}
+        lastSearch={lastSearch}
+      />
+    </div>
+  );
 }
 
 export default MainSearch;
