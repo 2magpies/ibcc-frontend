98021b620229ad327b46257b9d21f93e7843ae51
2magpies <daphnemintz@yahoo.com>
2020-02-19 18:29:30 -0500

    Created DeleteUser.js

---
+++ src/components/ManageUser/DeleteUser.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+function DeleteUser(props) {
+    console.log(props);
+
+    const { match } = props;
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        getUser();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const url = `http://ibcc.herokuapp.com/users/${match.params.id}`;
+
+    function getUser() {
+        fetch(url)
+            .then(response => response.json())
+            .then(response => {
+                setEvent(response);
+            })
+            .catch(console.error);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                // window.location.href = 'http://localhost:3000';
+            })
+            .catch(console.error);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>{user.name}</h1>
+            <button type="submit">Delete</button>
+        </form>
+    );
+}
+
+export default DeleteUser;
