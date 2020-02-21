56a3ec5788b5d13284af65131199878dcccf4db9
Mysta3 <myshawnestallings@gmail.com>
2020-02-20 16:32:22 -0800

    changed p tags > Card.Text components, add conditional for 'Found results string'

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
-import {CardColumns, Card} from 'react-bootstrap'
-import './Results.css'
+import { CardColumns, Card } from 'react-bootstrap';
+import './Results.css';
 
 function Results(props) {
   const { newResults, lastSearch } = props;
 
-
   return (
     <div id="resultsBox">
       <div id="resultsHeader">
-      <h5>Found {newResults.length} results for '{lastSearch}'</h5>
+        <h5>
+          Found {newResults.length} results for '{lastSearch || 'all'}'
+        </h5>
       </div>
-      <div id='resultsGrid'>
-      <CardColumns>
-        {newResults.map(event => (
-          <Card key={event._id}>
-            <Link to={`/${event._id}`}>
-              <Card.Img variant="top" src={event.imageUrl} alt={event.name} />
-              <Card.Body>
-                <Card.Title>{event.name}</Card.Title>
-                <Card.Text>
-                  <p>{moment(event.date).format('ddd, MMM Do YYYY')}</p>
-                  <p>{event.location}</p>
-                </Card.Text>
-              </Card.Body>
-            </Link>
-          </Card>
-        ))}
-      </CardColumns>
+      <div id="resultsGrid">
+        <CardColumns>
+          {newResults.map(event => (
+            <Card key={event._id}>
+              <Link to={`/${event._id}`}>
+                <Card.Img variant="top" src={event.imageUrl} alt={event.name} />
+                <Card.Body>
+                  <Card.Title>{event.name}</Card.Title>
+                  <Card.Text>
+                    {moment(event.date).format('ddd, MMM Do YYYY')}
+                  </Card.Text>
+                  <Card.Text>{event.location}</Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          ))}
+        </CardColumns>
       </div>
     </div>
   );
 }
 
 export default Results;
-
